8279a635f3791b322d6d489bdc74a39d28e20f9f
Tim Savage <tim@savage.company>
2020-06-23 13:50:21 +1000

    Add "error" command that raises an error

--- sample/cli.py
+++ sample/cli.py
@@ -3,5 +3,13 @@ from pyapp.app import CliApplication
 APP = CliApplication()
 
 
+@APP.command()
+def error(opts):
+    """
+    Generate an error
+    """
+    raise Hell
+
+
 def main(args=None):
     APP.dispatch(args)
